9ae9b4f3cf0d2870c17beec8a393e76cc89afed4
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-05-06 18:24:46 +0500

    Update tg_bot.py
    исправил недочеты

--- tg_bot.py
+++ tg_bot.py
@@ -15,32 +15,30 @@ def main():
     env = Env()
     env.read_env()
 
-    BOT_TOKEN = env('BOT_TOKEN')
-    bot = telebot.TeleBot(token=BOT_TOKEN)
+    bot_token = env('BOT_TOKEN')
+    bot = telebot.TeleBot(token=bot_token)
     logger.setLevel(logging.WARNING)
     logger.addHandler(
         LogHandler(tg_bot=bot, chat_id=env('CHAT_ID'))
     )
     logger.warning("TG_bot запущен")
 
-
     @bot.message_handler(commands=['start'])
     def process_start_command(message):
         bot.send_message(chat_id=message.chat.id, text='Zzz...')
 
-
     @bot.message_handler(commands=['help'])
     def process_help_command(message):
         bot.send_message(chat_id=message.chat.id,
-                         text='Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение'
-                        )
-
+                         text='Напиши мне что-нибудь и в ответ '
+                              'я пришлю тебе твое сообщение'
+                         )
 
     @bot.message_handler(content_types=['text'])
     def send_answer(message):
-        serialized_answer = detect_intent_text(env('DIALOG_ID'), message.from_user.id, message.text, flag=False)
-        if serialized_answer is not None:
-            bot.send_message(chat_id=message.chat.id, text=serialized_answer['answer'])
+        serialized_answer = detect_intent_text(env('DIALOG_ID'), message.from_user.id, message.text)
+        bot.send_message(chat_id=message.chat.id, text=serialized_answer['answer'])
+
     while True:
         try:
             bot.polling()
