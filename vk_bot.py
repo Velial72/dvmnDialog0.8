a67069dd32eed181bf01582bbc206715668e9823
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-05-06 18:25:15 +0500

    Update vk_bot.py
    исправил недочеты

--- vk_bot.py
+++ vk_bot.py
@@ -6,7 +6,6 @@ from time import sleep
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
-from tg_bot import bot
 from logs import LogHandler
 
 from dialog import detect_intent_text
@@ -19,23 +18,23 @@ def send_answer(answer, user_id, vk_api):
     vk_api.messages.send(
         user_id=user_id,
         message=answer,
-        random_id=random.randint(1,1000)
+        random_id=random.randint(1, 1000)
     )
 
 
 def main():
     env = Env()
     env.read_env()
-    
-    BOT_TOKEN = env('BOT_TOKEN')
-    bot = telebot.TeleBot(token=BOT_TOKEN)
+
+    bot_token = env('BOT_TOKEN')
+    bot = telebot.TeleBot(token=bot_token)
     logger.setLevel(logging.WARNING)
     logger.addHandler(
         LogHandler(tg_bot=bot, chat_id=env('CHAT_ID'))
     )
     logger.warning("VK_bot запущен")
-    VK_TOKEN = env('VK_TOKEN')
-    vk_session = vk.VkApi(token=VK_TOKEN)
+    vk_token = env('VK_TOKEN')
+    vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     while True:
         try:
@@ -43,8 +42,10 @@ def main():
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                     serialized_answer = detect_intent_text(env('DIALOG_ID'), event.user_id, event.text)
-                    if serialized_answer is not None:
+                    if serialized_answer['fallback'] == False:
                         send_answer(serialized_answer['answer'], event.user_id, vk_api)
+                    else:
+                        pass
         except requests.exceptions.ConnectionError:
             logging.exception("VK_bot упал с ошибкой")
             sleep(120)
