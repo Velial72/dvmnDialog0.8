ac6fbb71e0324aa093346c130a52b4349f668fa1
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-05-07 16:50:07 +0500

    Update creation_intent.py

--- creation_intent.py
+++ creation_intent.py
@@ -3,35 +3,37 @@ from google.cloud import dialogflow
 from environs import Env
 
 
+env = Env()
+env.read_env()
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+
+
 def main():
-    env = Env()
-    env.read_env()
-
-    def create_intent(project_id, display_name, training_phrases_parts, message_texts):
-        """Create an intent of the given intent type."""
-
-        intents_client = dialogflow.IntentsClient()
-
-        parent = dialogflow.AgentsClient.agent_path(project_id)
-        training_phrases = []
-        for training_phrases_part in training_phrases_parts:
-            part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-            # Here we create a new training phrase for each provided part.
-            training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
-            training_phrases.append(training_phrase)
-
-        text = dialogflow.Intent.Message.Text(text=[message_texts])
-
-        message = dialogflow.Intent.Message(text=text)
-
-        intent = dialogflow.Intent(
-            display_name=display_name, training_phrases=training_phrases, messages=[message]
-        )
-
-        response = intents_client.create_intent(
-            request={"parent": parent, "intent": intent}
-        )
-
     with open(env('PATH_TO_FILE'), "r", encoding="utf-8") as file:
         questions_json = file.read()
     questions = json.loads(questions_json)
